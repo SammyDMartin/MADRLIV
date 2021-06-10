d2a57533a913e440c4f341cf27656015d9c5957f
Sammy Martin <sammydmartin39@gmail.com>
2021-06-10 12:40:51 +0100

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -25,8 +25,8 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         #self.seed = torch.manual_seed(seed)
-        seed = torch.random.seed()
-        self.seed = torch.manual_seed(seed)
+        self.seed = torch.random.seed()
+        #self.seed = torch.manual_seed(seed)
         #print(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
